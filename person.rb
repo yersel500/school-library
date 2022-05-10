426e7c943c4346474834791b7ff741eea87ef61d
yersel500 <yersel500@gmail.com>
2022-05-10 10:20:02 -0500

    Person Inherit from Nameable

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -19,5 +22,9 @@ class Person
 
   def can_use_services?
     true if of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
