0eeee5a7621f7ccaae1a3218589dedfa34198de1
yersel500 <yersel500@gmail.com>
2022-05-10 15:27:57 -0500

    Create Classroom class

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_accessor :clasroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
