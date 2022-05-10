5b9834db5ae79813e59dc7011edaa0586e946d2a
yersel500 <yersel500@gmail.com>
2022-05-10 16:12:27 -0500

    Create many-to-many btw Person, Book and Rental.

--- book.rb
+++ book.rb
@@ -1,8 +1,14 @@
 class Book
   attr_accessor :title, :autor
+  attr_reader :rentals
 
   def initialize(title, autor)
     @title = title
     @autor = autor
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
   end
 end
