af722539fa28c7e1e040865395e8e4e2b49ccc6a
yersel500 <yersel500@gmail.com>
2022-05-13 00:30:21 -0500

    Add filter rentals by id

--- app.rb
+++ app.rb
@@ -1,13 +1,16 @@
 require './book'
 require './teacher'
 require './student'
+require './rental'
+require './person'
 
 class App
-  attr_reader :books, :people
+  attr_reader :books, :people, :rentals
 
   def initialize
     @books = []
     @people = []
+    @rentals = []
   end
 
   def list_of_books
@@ -33,12 +36,23 @@ class App
   def create_rental(date, book_id, person_id)
     @rentals << Rental.new(date, @books[book_id], @people[person_id])
   end
+
+  def list_rentals_byid(person_id)
+    @rentals.select { |rent| rent.person.id == person_id }
+  end
 end
 
 my_app = App.new
 my_app.create_book('Algebra', 'Baldor')
 my_app.create_book('Trigonometry', 'Bigote')
+my_app.create_book('Economy', 'Soto')
 p my_app.books
 my_app.create_teacher(35, 'programming', 'Bray')
 my_app.create_teacher(24, 'bussiness', 'Diana')
 p my_app.people
+my_app.create_rental('5/12/2022', 0, 1)
+my_app.create_rental('5/12/2022', 2, 1)
+my_app.create_rental('5/12/2022', 1, 0)
+p my_app.rentals
+p 'Rentals by id'
+p my_app.list_rentals_byid(1)
---
+++ main.rb
@@ -0,0 +1,3 @@
+class Main
+  def main; end
+end
