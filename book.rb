1e6c9a95e0dbac92a0d5e6d21c2750b8b9b0d542
Ivan Silva <ivankuartz@hotmail.com>
2022-05-17 18:05:02 -0300

    Main classes and functions

--- book.rb
+++ book.rb
@@ -1,6 +1,5 @@
 class Book
-  attr_accessor :title, :author
-  attr_reader :rentals
+  attr_accessor :rentals, :title, :author
 
   def initialize(title, author)
     @title = title
@@ -8,7 +7,16 @@ class Book
     @rentals = []
   end
 
-  def add_rental(person, date)
-    Rental.new(date, self, person)
+  def add_rental(date, person)
+    rental = Rental.new(date, self, person)
+    @rentals << rental unless @rentals.include?(rental)
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'title' => @title,
+      'author' => @author
+    }.to_json(*args)
   end
 end
--- student.rb
+++ student.rb
@@ -1,20 +1,29 @@
 require './person'
 
 class Student < Person
-  attr_reader :classroom, :category
+  attr_accessor :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(classroom, age, name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
-    @category = 'Student'
   end
 
   def play_hooky
     "¯\(ツ)/¯"
   end
 
-  def classroom=(classroom)
+  def change_classroom=(classroom)
     @classroom = classroom
-    classroom.students.push(self) unless classsroom.students.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'name' => @name,
+      'age' => @age,
+      'parent_permission' => @parent_permission
+    }.to_json(*args)
   end
 end
