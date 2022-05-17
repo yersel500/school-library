8f4cb8c50189045a843b0b4b50d24bcc6dd012e4
Ivan Silva <ivankuartz@hotmail.com>
2022-05-17 18:03:57 -0300

    Initiating refactoring of code

--- app.rb
+++ app.rb
@@ -1,43 +1,118 @@
 require './book'
-require './teacher'
 require './student'
+require './teacher'
 require './rental'
 require './person'
+require './checks'
+require_relative 'create_book'
+require_relative 'create_person'
+require './create_rental'
+require 'json'
 
 class App
-  attr_reader :books, :people, :rentals
+  include Checks
+  attr_accessor :books, :people
 
   def initialize
-    @books = []
     @people = []
-    @rentals = []
+    @books = []
+    @rents = []
+    @create_book = CreateBook.new(@books)
+    @create_person = CreatePerson.new(@people)
+    @create_rental = CreateRental.new(@rents, @books, @people)
   end
 
-  def list_of_books
-    @books
+  def run(choice)
+    case choice
+    when 1
+      list_books
+    when 2
+      list_people
+    when 3
+      @create_person.create_person
+    when 4
+      @create_book.create_book
+    when 5
+      @create_rental.create_rental
+    when 6
+      list_rental
+    end
   end
 
-  def list_of_people
-    @people
+  def load_books
+    if File.exist?('books.json')
+      data = JSON.parse(File.read('books.json'), create_additions: true)
+      data.each do |book|
+        @books.push(Book.new(book['title'], book['author']))
+      end
+    else
+      []
+    end
   end
 
-  def create_book(title, author)
-    @books << Book.new(title, author)
+  def load_people
+    if File.exist?('people.json')
+      data = JSON.parse(File.read('people.json'), create_additions: true)
+      data.each do |person|
+        case person['json_class']
+        when 'Student'
+          student = Student.new(nil, person['age'], name: person['name'],
+                                                    parent_permission: person['parent_permission'])
+
+          student.id = person['id']
+          @people.push(student)
+        when 'Teacher'
+          teacher = Teacher.new(person['specialization'], person['age'], name: person['name'])
+          teacher.id = person['id']
+          @people.push(teacher)
+        end
+      end
+    else
+      []
+    end
   end
 
-  def create_teacher(age, specialization, name)
-    @people << Teacher.new(age, specialization, name)
+  def load_files
+    load_books
+    load_people
+    load_rentals
   end
 
-  def create_student(age, classroom, name, parent_permission)
-    @people << Student.new(age, classroom, name, parent_permission: parent_permission)
+  def load_rentals
+    if File.exist?('rentals.json')
+      data = JSON.parse(File.read('rentals.json'), create_additions: true)
+      data.map do |rentals|
+        person = @people.find { |people| people.id == rentals['id_people'] }
+        book = @books.find { |books| books.title == rentals['book_title'] }
+        @rents.push(Rental.new(rentals['date'], book, person))
+      end
+    else
+      []
+    end
   end
 
-  def create_rental(date, book_id, person_id)
-    @rentals << Rental.new(date, @books[book_id], @people[person_id])
+  def list_books
+    @books.each { |book| puts "Title: \"#{book.title}\", Author: \"#{book.author}\"" }
   end
 
-  def list_rentals_byid(person_id)
-    @rentals.select { |rent| rent.person.id == person_id }
+  def list_people
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end
+
+  def list_rental
+    print "ID of person:\s"
+    id = gets.chomp.to_i
+    puts "Rentals:\s"
+    @rents.map do |rental|
+      puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}\n\n" if id == rental.person.id
+    end
+  end
+
+  def save_files
+    File.write('books.json', JSON.generate(@books))
+    File.write('people.json', JSON.generate(@people))
+    File.write('rentals.json', JSON.generate(@rents))
   end
 end
--- main.rb
+++ main.rb
@@ -1,58 +1,28 @@
+#!/usr/bin/env ruby
 require './app'
-require './list_books'
-require './create_person'
-require './list_people'
-require './create_book'
-require './create_rental'
-
-def options
-  puts "
-  Please choose an option by enterin a number:\n
-  1- List all books
-  2- List all people
-  3- Create a person
-  4- Create a book
-  5- Create a rental
-  6- List all rental for a given id
-  7- exit"
-
-  gets.chomp.to_i
-end
-
-def list_rentals_byid(library)
-  print 'ID of person: '
-  person_id = gets.chomp.to_i
-  library.list_rentals_byid(person_id).each_with_index do |rental, idx|
-    puts "#{idx}) Date: #{rental.date}, Book #{rental.book.title} by #{rental.book.author} "
-  end
-end
-
-def execute(library)
+require 'json'
+def main
+  puts 'Welcome to school Library App!'
+  app = App.new
+  app.load_files
   loop do
-    case options
-    when 1
-      ListBooks.new(library).list_of_books
-    when 2
-      ListPeople.new(library).list_of_people
-    when 3
-      CreatePerson.new(library).create_person
-    when 4
-      CreateBook.new(library).create_book
-    when 5
-      CreateRental.new(library).create_rental
-    when 6
-      list_rentals_byid(library)
-    else
-      puts 'Thanks for using this app'
+    puts "Please choose an option by entering a number:
+        1 - List all books
+        2 - List all people
+        3 - Create a person
+        4 - Create a book
+        5 - Create a rental
+        6 - List all rentals for a given person id
+        7 - Exit"
+    choice = gets.chomp.to_i
+
+    if choice == 7
+      app.save_files
       break
     end
+    app.run(choice)
   end
-end
 
-def main
-  library = App.new
-  puts "Welcome to School Library App\n"
-  execute(library)
+  puts "Thank you for using this app!\n\n"
 end
-
 main
