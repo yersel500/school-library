cd49927b3b4fd5bf4cb921d549909b33672440dd
Ivan Silva <ivankuartz@hotmail.com>
2022-05-18 17:45:53 -0300

    Added decorator class tests

---
+++ spec/decorator_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../decorator'
+require_relative '../nameable'
+
+describe Decorator do
+    before(:each) do
+        @decorator = Decorator.new('Killian')
+    end
+
+    it 'Tests if decorator has a neameable' do
+        expect(@decorator.nameable).to eq('Killian')
+    end
+end
